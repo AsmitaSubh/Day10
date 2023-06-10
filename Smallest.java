8e757b0c5055a413d65ba3f1137e3f25e8bb72c8
AsmitaSubh <saswain92@gmail.com>
2023-06-10 18:11:46 +0530

    Smallest

---
+++ Smallest.java
@@ -0,0 +1,16 @@
+public class Smallest 
+{
+        public static void main(String[] args) 
+        {
+            int[] array = {13,6,9,2,12};
+            int min = array[0];
+            for(int i=1;i<array.length;i++)
+            {
+                if(array[i]<min){
+                    min = array[i];
+                }
+            }
+            System.out.println("the smallest element in the array : " + min);
+        }
+    }
+
