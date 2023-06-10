3333876bc5f48d004f656cb82c4bc8729b290cc5
AsmitaSubh <saswain92@gmail.com>
2023-06-10 18:15:44 +0530

    Odd

---
+++ Odd.java
@@ -0,0 +1,10 @@
+public class Odd
+{
+    public static void main(String[] args)
+    {
+        int[] array =  {1,2,3,4,5};
+        for(int i=0;i<array.length;i+=2){
+            System.out.println(array[i]);
+        }
+    }
+}
